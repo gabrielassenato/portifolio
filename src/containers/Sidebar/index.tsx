3864271f859b777a8f68da196479a6d9fc98c819
Gabriel Assenato <gabrielassenato@gmail.com>
2024-02-21 23:58:30 -0300

    adicionando sidebar sobre mim e projetos

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/gabrielassenato.png" />
+    <Titulo>Gabriel Assenato</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      soluta, delectus numquam tempora optio et aliquam, nisi odio aspernatur
+      aut deserunt quae culpa, sint cum sit excepturi quasi architecto suscipit.
+    </p>
+  </section>
+)
+
+export default Sobre
