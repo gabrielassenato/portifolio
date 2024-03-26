93ec8eb1550bc9e285c6cb54741694b108aa9e76
Gabriel Assenato <gabrielassenato@gmail.com>
2024-03-26 13:06:25 -0300

    finalizando o projeto

--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -5,18 +5,23 @@ export const Descricao = styled(ParagrafoEstilo)`
   margin-top: 24px;
   margin-bottom: 40px;
 `
-
 export const BotaoTema = styled.button`
   border-radius: 12px;
   padding: 8px;
-  color: #eee;
-  background-color: #282a35;
-  font-weight: bold;
+  color: ${(props) => props.theme.corDeFundo};
   font-size: 10px;
+  font-weight: bold;
+  background-color: ${(props) => props.theme.corPrincipal};
   cursor: pointer;
 `
+
 export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+
+  @media (max-width: 768px) {
+    margin-bottom: 40px;
+    text-align: center;
+  }
 `
